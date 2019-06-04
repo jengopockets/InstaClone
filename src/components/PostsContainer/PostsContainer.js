16f6f179490c497a3fa9bfb5392e99ed288f80ed
Jengopockets <jengopockets@gmail.com>
2019-06-04 13:49:19 -0400

    Backtracking

---
+++ src/components/PostsContainer/PostsContainer.js
@@ -0,0 +1,13 @@
+import React from './node_modules/react';
+import Post from './Post';
+import './Posts.css';
+
+const PostsContainer = props => {
+  return (
+    <div className="posts-container-wrapper">
+      {props.posts.map(p => <Post key={p.imageUrl} post={p} />)}
+    </div>
+  );
+};
+
+export default PostsContainer;
