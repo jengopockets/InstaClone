676bd07cbc1a49b0654f04fa8a9ad14bed5d7a62
Jengopockets <jengopockets@gmail.com>
2019-06-05 17:27:40 -0400

    Created with Authenticate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import PostsPage from './components/PostsContainer/PostsPage';
+import withAuthenticate from './authentication/Authenticate';
 import './App.css';
 
+const ComponentFromWithAuthenticate = () => <div>< PostsPage/></div>;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        < PostsPage />
-      </div>
+      <ComponentFromWithAuthenticate />
     ); 
   }
 }
 
+
 export default App;
+
