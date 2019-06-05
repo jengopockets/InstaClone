1969fdefbaf28ba628f466914549aa5858ad2113
Jengopockets <jengopockets@gmail.com>
2019-06-05 16:28:37 -0400

    PostsPage Created and App Refactored

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import React, { Component } from 'react';
+import PostsPage from './components/PostsContainer/PostsPage';
 import './App.css';
-import dummyData from './dummy-data';
-import PostsContainer from './components/PostsContainer/PostsContainer';
-import SearchBar from './components/SearchBar/SearchBarContainer';
+
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: [],
-      filteredPosts: []
-    };
-  }
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-  searchPostsHandler = e => {
-    const posts = this.state.posts.filter(p => 
-      (p.username.includes(e.target.value)) 
-    );
-    this.setState({ filteredPosts: posts });
-  };
   render() {
     return (
       <div className="App">
-        <SearchBar
-          searchTerm={this.state.searchTerm}
-          searchPosts={this.searchPostsHandler}
-        />
-        <PostsContainer
-          posts={
-            this.state.filteredPosts.length > 0
-              ? this.state.filteredPosts
-              : this.state.posts
-          }
-        />
+        < PostsPage />
       </div>
-    );
+    ); 
   }
 }
 
